2c90413e57a6e6c5da93ea2fa50fd490f86acf99
douweknook <douweknook@gmail.com>
2016-11-22 14:52:29 +0100

    added data to index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app 		= express()
 // Require our modules
 const scraper 	= require(__dirname + '/modules/scraper')
 const db 		= require(__dirname + '/modules/database')
+const weather 	= require(__dirname + '/modules/compare')
 
 // Set view engine to pug
 app.set('view engine', 'pug')
@@ -18,20 +19,21 @@ app.use(express.static(__dirname+'/static'))
 // Index route
 app.get('/', (req, res) => {
 	// Get latest prediction results from database (= today's results)
-	db.Preditions.findAll({
+	db.Compare.findAll({
 		limit: 1,							// find only one database entry
 		order: [ ['createdAt', 'DESC'] ] 	// make sure it is the last added entry
 	}).then( (result) => {
 		console.log('Data hieronder wordt naar front end gestuurd.')
-		console.log(result)
+		console.log(result.dataValues)
 		// Render index with these result
-		res.render('index', {result: result})
+		res.render('index', {result: result.dataValues})
 	})
 })
 
 // Run scraper once every 24 hours
 // setInterval( () => {
 	scraper.scrape()
+	weather.compare()
 // }, 8640000) // 100x60x60x26 milliseconds equals 24 hours.
 
 // app listens on localhost 8000
